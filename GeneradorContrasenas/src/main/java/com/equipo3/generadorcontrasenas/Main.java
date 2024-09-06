e0fc4abcbbcb6cfc598d72a20a942e680aa6ca5a
Saul Neri <saul.neri247498@potros.itson.edu.mx>
2024-09-05 23:50:50 -0600

    Se anadieron 2 opciones: 'mostrar contrasenas guardadas' y 'guardar contrasenas', faltan detalles como crear el evaluador de contrasenas

--- GeneradorContrasenas/src/main/java/com/equipo3/generadorcontrasenas/Main.java
+++ GeneradorContrasenas/src/main/java/com/equipo3/generadorcontrasenas/Main.java
@@ -10,39 +10,8 @@ public class Main {
     
     public static final String PATRON_CONTRASENAS = "^[a-zA-Z0-9?_=\\-$%&!]+$";
     
-    public static void muestraOpciones() {
-//        System.out.println("");
-//        System.out.println("1. Generar Contrasena Encriptada");
-//        System.out.println("2. Evaluar Contrasena");
-//        System.out.println("3. Mostrar Contrasenas Guardadas");
-//        System.out.println("0. Salir");
-//        System.out.println("");
-    }
-    
-    public static void muestraAlgoritmos(Iterator<String> lista) {
-        int i=0;
-        while (lista.hasNext()) {
-            System.out.printf(String.format("%d. %s\n", i, lista.next()));
-            i++;
-        }
-    }
-    
     public static void main(String[] args) {
         Menu menu = new Menu();
         menu.desplegarMenu();
-//        GeneradorContrasenas generadorContrasenas = new GeneradorContrasenas();
-//        Encriptador encriptador;
-//        
-//        Pattern patronContrasenaCorrecta = Pattern.compile(Main.PATRON_CONTRASENAS);
-//  
-//        Scanner entrada = new Scanner(System.in);
-//        
-//        boolean buclePrincipal = true;
-//        
-//        while (buclePrincipal) {
-//            Main.muestraOpciones();
-//            System.out.print(">>> ");
-//            int opcion = entrada.nextInt();
-//        }
     }
 }
