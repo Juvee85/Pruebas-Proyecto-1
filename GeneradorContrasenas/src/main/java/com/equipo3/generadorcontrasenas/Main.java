e4712ab1efd981d479caf129b19acf7fcf0d7264
SaulNeri12 <saul.neri247498@potros.itson.edu.mx>
2024-09-07 12:43:49 -0600

    tests
    tests

--- GeneradorContrasenas/src/main/java/com/equipo3/generadorcontrasenas/Main.java
+++ GeneradorContrasenas/src/main/java/com/equipo3/generadorcontrasenas/Main.java
@@ -7,9 +7,6 @@ import java.util.Iterator;
  * @author Equipo3
  */
 public class Main {
-    
-    public static final String PATRON_CONTRASENAS = "^[a-zA-Z0-9?_=\\-$%&!]+$";
-    
     public static void main(String[] args) {
         Menu menu = new Menu();
         menu.desplegarMenu();
