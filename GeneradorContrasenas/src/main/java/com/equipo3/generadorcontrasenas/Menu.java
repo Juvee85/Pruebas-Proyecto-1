4ed166919c12b9aa44fd871915b1c685e0e91ab3
SaulNeri12 <saul.neri247498@potros.itson.edu.mx>
2024-09-08 00:52:31 -0600

    Validaciones Try-Catch
    Se anadieron validaciones a datos erroneos con uso de excepciones para cada entrada en el programa

--- GeneradorContrasenas/src/main/java/com/equipo3/generadorcontrasenas/Menu.java
+++ GeneradorContrasenas/src/main/java/com/equipo3/generadorcontrasenas/Menu.java
@@ -4,8 +4,11 @@
 package com.equipo3.generadorcontrasenas;
 
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Clase que despliega el menu de consola y maneja la selección de opciones del
@@ -17,6 +20,8 @@ public class Menu {
 
     private IGeneradorContrasenas generador;
     private IEvaluadorContrasenas evaluador;
+    private static final Pattern PATRON_TEXTO = Pattern.compile("^[a-zA-Z0-9\\S?_=\\-@$%&!^#]+$");
+    private static final Pattern PATRON_ESPACIOS = Pattern.compile("\\s");
     
     private Map<String, String> contrasenasGuardadas;
     
@@ -45,24 +50,31 @@ public class Menu {
             System.out.println("\t4. Guardar Contrasena");
             System.out.println("\t0.Salir\n");
 
-            System.out.print(">>> ");
-            int opcion = tec.nextInt();
+            try {
+                System.out.print(">>> ");
+                int opcion = tec.nextInt();
 
-            //Ejecuta la opcion solicitada
-            switch (opcion) {
-                case 1 ->
-                    generarContrasena();
-                case 2 ->
-                    evaluarContrasena();
-                case 3 ->
-                    mostrarContrasenas();
-                case 4 -> 
-                    guardarContrasena();
-                case 0 ->
-                    ejecutar = false; //Termina el proceso
-                default ->
-                    System.out.println("Opción no valida, "
-                            + "ingrese una del 0 al 3");
+                //Ejecuta la opcion solicitada
+                switch (opcion) {
+                    case 1 ->
+                        generarContrasena();
+                    case 2 ->
+                        evaluarContrasena();
+                    case 3 ->
+                        mostrarContrasenas();
+                    case 4 -> 
+                        guardarContrasena();
+                    case 0 -> {
+                        System.out.println("[=*=] Saliendo...");
+                        ejecutar = false; //Termina el proceso
+                    }
+                    default ->
+                        System.out.println("Opción no valida, "
+                                + "ingrese una del 0 al 3");
+                }
+            } catch (InputMismatchException ex) {
+                System.out.println("[!] Ingresa una opcion...");
+                tec.nextLine();
             }
         } while (ejecutar);
     }
@@ -71,23 +83,26 @@ public class Menu {
      * Ejecuta la generación de una contraseña segura
      */
     private void generarContrasena() {
-        System.out.println("Ingrese la longitud que desea para su contrasena (longitud minima: 8): ");
-        
         boolean entradaValida = false;
         
         int longitud = 8;
         
         while (!entradaValida) {
-            System.out.print(">>> ");
-            
-            longitud = tec.nextInt();
-            
-            if (longitud < 8) {
-                System.out.println("[!] El tamano de la contrasena debe tener una longitud minima de 8 caracteres...");
-                continue;
+            try {
+                System.out.print("Ingrese la longitud que desea para su contrasena (longitud minima: 8): ");
+
+                longitud = tec.nextInt();
+
+                if (longitud < 8 || longitud > 100) {
+                    System.out.println("[!] El tamano de la contrasena debe tener una longitud minima de 8 y un maximo de 100 caracteres...");
+                    continue;
+                }
+
+                entradaValida = true;
+            } catch (InputMismatchException ex) {
+                System.out.println("[!] Ingresa un numero...");
+                tec.nextLine();
             }
-            
-            entradaValida = true;
         }
        
         String contrasena = generador.generarContrasena(longitud);
@@ -101,7 +116,34 @@ public class Menu {
      */
     private void evaluarContrasena() {
         System.out.println("Ingrese la contraseña que desea evaluar");
-        String contrasena = tec.next();
+        
+        String contrasena = "";
+        
+        boolean entradaValida = false;
+        
+        while (!entradaValida) {
+            try {
+                contrasena = tec.next();
+
+                if (this.contieneEspacios(contrasena)) {
+                    throw new InputMismatchException("[!] La contrasena no debe contener espacios...");
+                }
+                
+                if (contrasena.length() < 4 || contrasena.length() > 100) {
+                    throw new InputMismatchException("[!] La contrasena debe contener un minimo de 4 caracteres y un maximo de 100");
+                }
+                
+                if (!this.entradaTextoValida(contrasena) || (contrasena.isBlank() || contrasena.isBlank())) {
+                    throw new InputMismatchException("[!] La contrasena debe contener caracteres ASCII y no debe estar vacia...");
+                }
+                
+                entradaValida = true;
+            } catch (InputMismatchException ex) {
+                System.out.println(ex.getMessage());
+                tec.nextLine();
+            }
+        }
+    
 
         IEvaluadorContrasenas evaluadorSecuencial = new EvaluadorContrasenasSecuencial();
         
@@ -121,28 +163,54 @@ public class Menu {
         String nombreContrasena = "", contrasena = "";
         
         while (!entradaValida) {
-            System.out.print("Ingresa el nombre con el que deseas guardar la contrasena[minimo de 4 y menor a 20 caracteres]: ");
-            nombreContrasena = tec.next();
-            
-            if (nombreContrasena.length() < 4 || nombreContrasena.length() > 20 || nombreContrasena.isBlank() || nombreContrasena.isEmpty()) {
-                System.out.println("[!] El nombre de la contrasena debe tener al menos 4 y un maximo de 20 caracteres...");
-                continue;
+            try {
+                System.out.print("Ingresa el nombre con el que deseas guardar la contrasena[minimo de 4 y menor a 20 caracteres]: ");
+                nombreContrasena = tec.next();
+
+                if (nombreContrasena.isBlank() || nombreContrasena.isEmpty()) {
+                    throw new InputMismatchException("[!] Ingresa el nombre de la contrasena...");
+                }
+                
+                if (!this.entradaTextoValida(nombreContrasena)) {
+                    throw new InputMismatchException("[!] El nombre solo debe contener caracteres ASCII...");
+                }
+                
+                if (nombreContrasena.length() < 4 || nombreContrasena.length() > 20) {
+                    throw new InputMismatchException("[!] El nombre de la contrasena debe tener al menos 4 y un maximo de 20 caracteres...");
+                }
+                
+                entradaValida = true;
+            } catch (InputMismatchException ex) {
+                System.out.println(ex.getMessage());
+                tec.nextLine();
             }
-            
-            entradaValida = true;
         }
+        
         
         entradaValida = false;
         
         while (!entradaValida) {
-            System.out.print("Ingresa la contrasena a guardar: ");
-            contrasena = tec.next();
-            
-            if (contrasena.isBlank() || contrasena.isEmpty()) {
-                System.out.println("[!] Por favor, ingresa una contrasena");
+            try {
+                System.out.print("Ingresa la contrasena a guardar: ");
+                contrasena = tec.next();
+
+                if (contrasena.isBlank() || contrasena.isEmpty()) {
+                    throw new InputMismatchException("[!] Por favor, ingresa una contrasena");
+                }
+                
+                if (contrasena.length() < 4 || contrasena.length() > 100) {
+                    throw new InputMismatchException("[!] La contrasena debe tener al menos 4 caracteres y un maximo de 100...");
+                }
+                
+                if (!this.entradaTextoValida(contrasena)) {
+                    throw new InputMismatchException("[!] La contrasena debe contener solo caracteres ASCII (letras, numeros, simbolos)...");
+                }
+
+                entradaValida = true;
+            } catch (InputMismatchException ex) {
+                System.out.println(ex.getMessage());
+                tec.nextLine();
             }
-            
-            entradaValida = true;
         }
         
         this.contrasenasGuardadas.put(nombreContrasena, contrasena);
@@ -166,6 +234,25 @@ public class Menu {
         regresar();
     }
 
+    private boolean entradaTextoValida(String texto) {
+        return PATRON_TEXTO.matcher(texto).matches();
+    }
+    
+    private boolean contieneEspacios(String texto) {
+        
+        if (this.entradaTextoValida(texto)) {
+            return false;
+        }
+        
+        for (char c: texto.toCharArray()) {
+            if (c == ' ') {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
     /**
      * Pausa la ejecución para esperar la entrada del usuario indicando que
      * desea continuar
